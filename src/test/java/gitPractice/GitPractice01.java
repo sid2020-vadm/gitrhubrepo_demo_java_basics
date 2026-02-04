b58f39b6accd348b3ff5ad6ac91753db79feafc0
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 17:08:18 +0530

    Refactor main method in GitPractice01
    Removed multiple print statements and updated the message.

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -2,12 +2,7 @@ package gitPractice;
 
 public class GitPractice01 {
     public static void main(String[] args){
-        System.out.println("login feature branch");
-        System.out.println("login feature branch");
-        System.out.println("login feature branch");
-        int a = 30;
-        System.out.println(a);
-        System.out.println("feature Branch");
+        System.out.println("setup feature");
 
     }
 }
