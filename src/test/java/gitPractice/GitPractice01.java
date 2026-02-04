fdbae99e7ee241f48e491936fdec6df7ef35311c
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 18:15:27 +0530

    added improvents

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -4,5 +4,6 @@ public class GitPractice01 {
     public static void main(String[] args){
         System.out.println("bug fix branch");
         System.out.println("added new code");
+        System.out.println("improved feature design");
     }
 }
