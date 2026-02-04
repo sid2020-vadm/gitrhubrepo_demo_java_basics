e78ccbd56f4818d0528f3b3f3e24c4e0dd8df49c
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 18:08:51 +0530

    new lined added

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -3,5 +3,6 @@ package gitPractice;
 public class GitPractice01 {
     public static void main(String[] args){
         System.out.println("bug fix branch");
+        System.out.println("added new code");
     }
 }
