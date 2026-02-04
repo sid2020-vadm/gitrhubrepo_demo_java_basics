282c5a4901a1299d88839c0c4a29f1cbba111465
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 16:42:34 +0530

    Duplicate print statements for login feature branch
    Added duplicate print statements for the login feature branch.

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -3,5 +3,7 @@ package gitPractice;
 public class GitPractice01 {
     public static void main(String[] args){
         System.out.println("login feature branch");
+        System.out.println("login feature branch");
+        System.out.println("login feature branch");
     }
 }
