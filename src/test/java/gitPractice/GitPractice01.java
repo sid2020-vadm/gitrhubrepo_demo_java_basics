503cb6c8cf4c611357bc0c390f54dc527b20fcd3
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 15:40:51 +0530

    update GitPractice01

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -2,6 +2,7 @@ package gitPractice;
 
 public class GitPractice01 {
     public static void main(String[] args){
+        System.out.println("added new line Master Branch");
         System.out.println("Git Pracice");
     }
 }
