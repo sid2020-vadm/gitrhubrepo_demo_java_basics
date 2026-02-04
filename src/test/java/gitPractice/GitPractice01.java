8b6c7a2715c2d64366d3d97f4aa90a26f29d69e6
sid2020-vadm <63712248+sid2020-vadm@users.noreply.github.com>
2026-02-04 16:41:33 +0530

    Update GitPractice01.java

--- src/test/java/gitPractice/GitPractice01.java
+++ src/test/java/gitPractice/GitPractice01.java
@@ -2,6 +2,6 @@ package gitPractice;
 
 public class GitPractice01 {
     public static void main(String[] args){
-        System.out.println("feature Branch");
+        System.out.println("login feature branch");
     }
 }
